94a205645ca4193986905b6210ba327b7f1e5959
ewhdtls0 <ewhdtls0@naver.com>
2024-06-08 23:44:45 +0900

    FEAT: 쿠폰 발행 정보 리스트 조회

--- src/main/java/my/coupon/advanced/repository/IssueRepository.java
+++ src/main/java/my/coupon/advanced/repository/IssueRepository.java
@@ -2,6 +2,12 @@ package my.coupon.advanced.repository;
 
 import my.coupon.advanced.domain.Issue;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface IssueRepository extends JpaRepository<Issue, Long> {
+
+    @Query("SELECT i FROM Issue i JOIN FETCH i.member JOIN FETCH i.coupon")
+    List<Issue> findAllWithEntries();
 }
--- src/main/java/my/coupon/advanced/service/IssueService.java
+++ src/main/java/my/coupon/advanced/service/IssueService.java
@@ -1,4 +1,26 @@
 package my.coupon.advanced.service;
 
+import lombok.RequiredArgsConstructor;
+import my.coupon.advanced.dto.IssueResponse;
+import my.coupon.advanced.repository.IssueRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class IssueService {
+    private final IssueRepository issueRepository;
+
+    /**
+     * 쿠폰 발행 정보 리스트 조회
+     * @return
+     */
+    public List<IssueResponse> findAllIssue() {
+        return issueRepository.findAllWithEntries()
+                .stream().map(IssueResponse::from).toList();
+    }
+
 }
